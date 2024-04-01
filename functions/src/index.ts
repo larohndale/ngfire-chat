101fe88624ce4ad1b5089623d8a4ee55748f50b5
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-03-31 21:03:53 -0400

    wip codelab docummentation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable max-len */
 /**
  * Import function triggers from their respective submodules:
  *
@@ -17,3 +19,34 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+// TODO(DEVELOPER): Import the Cloud Functions for
+// Firebase and the Firebase Admin modules here.
+
+// TODO(DEVELOPER): Write the addWelcomeMessage Function here.
+
+// TODO(DEVELOPER): Write the blurImages Function here.
+
+// TODO(DEVELOPER): Write the sendNotification Function here.
+
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+// Import and initialize the Firebase Admin SDK.
+admin.initializeApp();
+
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user: any) => {
+    functions.logger.log("A new user signed in for the first time.");
+    const fullName = user.displayName || "Anonymous";
+
+    // Saves the new welcome message into the database
+    // which then displays it in the FriendlyChat clients.
+    await admin.firestore().collection("messages").add({
+        name: "Firebase Bot",
+        profilePicUrl: "/images/firebase-logo.png", // Firebase logo
+        text: `${fullName} signed in for the first time! Welcome!`,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    functions.logger.log("Welcome message written to database.");
+});
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -29,43 +29,43 @@ import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    importProvidersFrom([
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideAuth(() => {
-            const auth = getAuth();
-            // if (location.hostname === 'localhost') {
-            //   connectAuthEmulator(auth, 'http://127.0.0.1:9099', {
-            //     disableWarnings: true,
-            //   });
-            // }
-            return auth;
-        }),
-        provideFirestore(() => {
-            const firestore = getFirestore();
-            // if (location.hostname === 'localhost') {
-            //   connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-            // }
-            return firestore;
-        }),
-        provideFunctions(() => {
-            const functions = getFunctions();
-            // if (location.hostname === 'localhost') {
-            //   connectFunctionsEmulator(functions, '127.0.0.1', 5001);
-            // }
-            return functions;
-        }),
-        provideStorage(() => {
-            const storage = getStorage();
-            // if (location.hostname === 'localhost') {
-            //   connectStorageEmulator(storage, '127.0.0.1', 5001);
-            // }
-            return storage;
-        }),
-        provideMessaging(() => {
-            return getMessaging();
-        }),
-    ]), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })]
+  importProvidersFrom([
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+      const auth = getAuth();
+      // if (location.hostname === 'localhost') {
+      //   connectAuthEmulator(auth, 'http://127.0.0.1:9099', {
+      //     disableWarnings: true,
+      //   });
+      // }
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      // if (location.hostname === 'localhost') {
+      //   connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+      // }
+      return firestore;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions();
+      // if (location.hostname === 'localhost') {
+      //   connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+      // }
+      return functions;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      // if (location.hostname === 'localhost') {
+      //   connectStorageEmulator(storage, '127.0.0.1', 5001);
+      // }
+      return storage;
+    }),
+    provideMessaging(() => {
+      return getMessaging();
+    }),
+  ]), provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000'
+  })]
 };
